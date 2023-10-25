2c98f656019b0fba7d4dee1cfea7d7e784d1d58e
agsobczak <s.agasobczak@gmail.com>
2023-10-25 19:18:53 +0200

    update1

--- src/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-export async function loadCountries(name) {
-  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-  const countries = await response.json();
-  return countries;
-}
-
-export function handleResults(results) {
-  // ...
-}
